f06d9cf7abb6a265d0a74ee344adea1bbfcf5b8e
Joseph Akaro <josephmmakaro@gmail.com>
2024-07-22 21:06:33 +0300

    task-0: Done

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,8 +1,28 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-
-export default [
-	{ files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-	{ languageOptions: { globals: globals.browser } },
-	pluginJs.configs.recommended,
-];
+module.exports = {
+	env: {
+		browser: false,
+		es6: true,
+		jest: true,
+	},
+	extends: ['airbnb-base', 'plugin:jest/all'],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	plugins: ['jest'],
+	rules: {
+		'no-console': 'off',
+		'no-shadow': 'off',
+		'no-restricted-syntax': ['error', 'LabeledStatement', 'WithStatement'],
+	},
+	overrides: [
+		{
+			files: ['*.js'],
+			excludedFiles: 'babel.config.js',
+		},
+	],
+};
