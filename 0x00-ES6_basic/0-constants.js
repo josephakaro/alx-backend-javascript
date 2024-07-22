cfc94ecb65370d9844d8e0bc6112f6af051a3e93
Joseph Akaro <josephmmakaro@gmail.com>
2024-07-22 21:08:00 +0300

    task-0: Debugged

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,10 +1,3 @@
-/**
- * @description : Modify the code to use const and let instead of var.
- * @param {string} : taskFirst - I prefer const when I can.
- * @param {string} : taskNext - But sometimes let is okay.
- * @returns {string} : taskFirst and taskNext
- */
-
 export function taskFirst() {
 	const task = 'I prefer const when I can.';
 	return task;
