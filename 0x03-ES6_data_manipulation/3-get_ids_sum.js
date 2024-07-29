234fc150f5431e9782cb9cda69677de3164b1ebc
Joseph Akaro <josephmmakaro@gmail.com>
2024-07-29 21:00:57 +0300

    Data Manipulation: Done #1

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,3 @@
-const getStudentIdsSum = (students) => {
-  students.reduce((preVal, cur) => preVal + cur.id, 0);
-};
+const getStudentIdsSum = (students) => students.reduce((preVal, cur) => preVal + cur.id, 0);
 
 export default getStudentIdsSum;
