bd690adb6b592ffab1852a6bd8fc35bf113d5549
Joseph Akaro <josephmmakaro@gmail.com>
2024-10-21 15:21:09 +0300

    Add: Eslint Fixes

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -5,7 +5,6 @@ const app = express();
 const PORT = 1245;
 const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
 
-
 const countStudents = (dataPath) => new Promise((resolve, reject) => {
   if (!dataPath) {
     reject(new Error('Cannot load the database'));
@@ -20,17 +19,11 @@ const countStudents = (dataPath) => new Promise((resolve, reject) => {
         const fileLines = data.toString('utf-8').trim().split('\n');
         const studentGroups = {};
         const dbFieldNames = fileLines[0].split(',');
-        const studentPropNames = dbFieldNames.slice(
-          0,
-          dbFieldNames.length - 1,
-        );
+        const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 
         for (const line of fileLines.slice(1)) {
           const studentRecord = line.split(',');
-          const studentPropValues = studentRecord.slice(
-            0,
-            studentRecord.length - 1,
-          );
+          const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
           const field = studentRecord[studentRecord.length - 1];
           if (!Object.keys(studentGroups).includes(field)) {
             studentGroups[field] = [];
@@ -47,11 +40,13 @@ const countStudents = (dataPath) => new Promise((resolve, reject) => {
         );
         reportParts.push(`Number of students: ${totalStudents}`);
         for (const [field, group] of Object.entries(studentGroups)) {
-          reportParts.push([
-            `Number of students in ${field}: ${group.length}.`,
-            'List:',
-            group.map((student) => student.firstname).join(', '),
-          ].join(' '));
+          reportParts.push(
+            [
+              `Number of students in ${field}: ${group.length}.`,
+              'List:',
+              group.map((student) => student.firstname).join(', '),
+            ].join(' '),
+          );
         }
         resolve(reportParts.join('\n'));
       }
